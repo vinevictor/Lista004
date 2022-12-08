f0dba51d19e0cbb13869549c0b160d514b57cbb0
Vinicius Victor <vine.victordev@gmail.com>
2022-12-08 01:56:09 -0300

    Funçao Verifica Repetição de Letras

--- script.js
+++ script.js
@@ -19,4 +19,38 @@ function ContainsRegExp(palavra, final) {
     }
 }
 console.log(ContainsIncludes('estudo', 'udo'));
-console.log(ContainsRegExp('estudo', 'udo'));
+console.log(ContainsRegExp('aula', 'ula'));
+// 02 ----------------------------------------------
+function ReturnRep(palavra) {
+    palavra = palavra.toLowerCase();
+    var reRepetidas = new RegExp('', 'g');
+    var reUnicas = new RegExp('', 'g');
+    var letras = [];
+    var regRepetidas = '';
+    var regUnicas = '';
+    letras = palavra.split('');
+    var _loop_1 = function (i) {
+        var verificaRepeticao = 0;
+        letras.forEach(function (letra) {
+            if (letra == palavra[i]) {
+                verificaRepeticao++;
+                letra = '0';
+            }
+        });
+        if (verificaRepeticao > 1 && !regRepetidas.includes(palavra[i])) {
+            regRepetidas = regRepetidas + palavra[i];
+        }
+        else if (verificaRepeticao < 2 && !regUnicas.includes(palavra[i])) {
+            regUnicas = regUnicas + palavra[i];
+        }
+    };
+    for (var i = 0; i < palavra.length; i++) {
+        _loop_1(i);
+    }
+    reRepetidas = new RegExp("[".concat(regRepetidas, "]"), 'g');
+    reUnicas = new RegExp("[".concat(regUnicas, "]"), 'g');
+    palavra = palavra.replace(reRepetidas, ')');
+    palavra = palavra.replace(reUnicas, '(');
+    return palavra;
+}
+console.log(ReturnRep('teste'));
--- script.ts
+++ script.ts
@@ -19,5 +19,42 @@ function ContainsRegExp(palavra: string, final: string): boolean {
     }
 }
 console.log(ContainsIncludes('estudo', 'udo'))
-console.log(ContainsRegExp('estudo', 'udo'))
+console.log(ContainsRegExp('aula', 'ula'))
 
+// 02 ----------------------------------------------
+
+
+function ReturnRep(palavra: string): string {
+    palavra = palavra.toLowerCase()
+    let reRepetidas = new RegExp('','g');
+    let reUnicas = new RegExp('','g');
+    let letras: string[] = []
+    let regRepetidas: string = '' 
+    let regUnicas: string = ''
+    letras = palavra.split('')
+    for(let i = 0; i < palavra.length; i++){
+        let verificaRepeticao: number = 0
+        letras.forEach(letra =>{
+            if(letra == palavra[i]){
+                verificaRepeticao++
+                letra = '0'
+            }
+        })
+        if(verificaRepeticao > 1 && !regRepetidas.includes(palavra[i])){
+            regRepetidas = regRepetidas + palavra[i];
+         }else if( verificaRepeticao < 2 && !regUnicas.includes(palavra[i])){
+             regUnicas = regUnicas + palavra[i]
+         }
+        }
+    
+        reRepetidas = new RegExp(`[${regRepetidas}]`,'g')
+        reUnicas = new RegExp(`[${regUnicas}]`,'g')
+        
+        palavra = palavra.replace(reRepetidas, ')')
+        palavra = palavra.replace(reUnicas, '(')
+
+        return palavra
+    }    
+
+
+console.log(ReturnRep('teste'))
